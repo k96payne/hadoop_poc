9211ab8cf79984c7070cec563633f64beee1a3d5
kpayne <kpayne@2018-COMP-1.Galatea.com>
2019-06-06 18:03:03 -0400

    connection to hdfs

--- hadoop-poc/src/main/java/org/galatea/hadooppoc/hdfs/HdfsApplication.java
+++ hadoop-poc/src/main/java/org/galatea/hadooppoc/hdfs/HdfsApplication.java
@@ -1,14 +1,23 @@
 package org.galatea.hadooppoc.hdfs;
 
-import lombok.SneakyThrows;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class HdfsApplication {
 
 	@SneakyThrows
 	public static void main(final String[] args) {
-
 		HdfsAccessor accessor = HdfsAccessor.newDefaultHdfsAccessor();
 		// accessor.getFileSystem().get
+		FileSystem fs = accessor.getFileSystem();
+		for (FileStatus f : fs.listStatus(new Path("/"))) {
+			System.out.println(f.getPath().getName());
+		}
 	}
 
 }
